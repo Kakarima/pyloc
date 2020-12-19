ed97e3b77bb6cc5cb4edbb43bd3dd4b875dbcf46
karima <karima.bacari@gmail.com>
2020-12-19 13:51:31 +0100

    Karima supprime gestion et laisse manager

--- PyLOC/gestion/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-        path('', views.gestion, name = 'gestion'),
-        path('vehicules/categories', views.vehicules_categories, name = 'vehicules_categories'),
-        path('disponibilites/categories', views.disponibilites_categories, name = 'disponibilites_categories'),
-        path('reservation/categories', views.reservation_categories, name = 'reservation_categories'),
-]
